f98c5060b0a2a6da16055670a8798b1ec9f04b0a
Raoul DIFFOUO <raoul.diffouo@prodigyfinance.com>
2019-02-08 12:19:34 +0200

    Add blog support using markdown and create first post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
+import { rhythm } from '../utils/typography'
 
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <h1>Hi people</h1>
+    {
+      data.allMarkdownRemark.edges.map(({ node: { id, frontmatter, excerpt } }) => (
+        <div key={id}>
+          <h3 style={{ marginBottom: `${rhythm(1 / 2)}` }}>
+            {frontmatter.title}{' '} <br />
+
+            <span style={{ color: '#bbb', fontSize: `${rhythm(1 / 2)}` }}>
+              {frontmatter.date}
+            </span>
+          </h3>
+          <p>{excerpt}</p>
+        </div>
+      ))
+    }
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
