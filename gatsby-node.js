5558eac5888a07275dcd210a2874b7f5af8f0e9d
Raoul DIFFOUO <raoul.diffouo@prodigyfinance.com>
2019-11-02 20:09:18 +0200

    feature: add post template and load content from ghost

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,31 +16,41 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  return graphql(`
+  const result = await graphql(`
     {
-      allMarkdownRemark {
+      allGhostPost(sort: { order: ASC, fields: published_at }) {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
   `)
-    .then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-          path: node.fields.slug,
-          component: path.resolve('./src/templates/blog-posts.js'),
-          context: {
-            slug: node.fields.slug,
-          }
-        })
-      })
+
+  if (result.errors) {
+    throw new Error(result.errors)
+  }
+
+  // Extract query results
+  const posts = result.data.allGhostPost.edges
+
+  // Load templates
+  const postTemplate = path.resolve('./src/templates/post.js')
+
+  // Create post pages
+  posts.forEach(({ node }) => {
+    node.url = `/${node.slug}/`
+
+    createPage({
+      path: node.url,
+      component: postTemplate,
+      context: {
+        slug: node.slug
+      }
     })
+  })
 }
