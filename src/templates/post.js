0d4f62e71d6a861588ee0801b272505b0f2d3325
Raoul DIFFOUO <raoul.diffouo@prodigyfinance.com>
2019-11-02 20:09:18 +0200

    feature: add post template and load content from ghost

---
+++ src/templates/post.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+
+/**
+ * Single post view (/:slug)
+ *
+ * This file is used to render a single post and loads all the content
+ *
+ */
+
+const Post = ({ data }) => {
+  const post = data.ghostPost
+
+  return (
+    <>
+      <Layout>
+        <article>
+          <h1>{post.title}</h1>
+          <section
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </article>
+      </Layout>
+    </>
+  )
+}
+
+Post.prototype = {
+  data: PropTypes.shape({
+    ghostPost: PropTypes.shape({
+      codeinjection_styles: PropTypes.object,
+      title: PropTypes.string.isRequired,
+      html: PropTypes.string.isRequired,
+      feature_image: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+  location: PropTypes.object.isRequired,
+}
+
+export default Post
+
+export const postQuery = graphql`
+  query($slug: String!) {
+    ghostPost(slug: { eq: $slug }) {
+      ...GhostPostFields
+    }
+  }
+`
---
+++ src/utils/fragments.js
@@ -0,0 +1,79 @@
+import { graphql } from 'gatsby'
+
+export const ghostPostFields = graphql`
+  fragment GhostPostFields on GhostPost {
+    # Main fields
+    id
+    title
+    slug
+    featured
+    feature_image
+    excerpt
+    custom_excerpt
+
+    # Dates formatted
+    created_at_pretty: created_at(formatString: "DD MMMM, YYYY")
+    published_at_pretty: published_at(formatString: "DD MMMM, YYYY")
+    updated_at_pretty: updated_at(formatString: "DD MMMM, YYYY")
+
+    # Dates unformatted
+    created_at
+    published_at
+    updated_at
+
+    # Authors
+    authors {
+      name
+      slug
+      bio
+      # email
+      profile_image
+      twitter
+      facebook
+      website
+    }
+    primary_author {
+      name
+      slug
+      bio
+      # email
+      profile_image
+      twitter
+      facebook
+      website
+    }
+
+    # Tags
+    primary_tag {
+      name
+      slug
+      description
+      feature_image
+      meta_description
+      meta_title
+      visibility
+    }
+    tags {
+      name
+      slug
+      description
+      feature_image
+      meta_description
+      meta_title
+      visibility
+    }
+
+    # Content
+    plaintext
+    html
+
+    # Additional fields
+    url
+    uuid
+    page
+    codeinjection_foot
+    codeinjection_head
+    codeinjection_styles
+    comment_id
+  }
+`
