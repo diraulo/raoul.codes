7c5051ce3bb02ffa001236e5792af13f25f36085
Raoul DIFFOUO <raoul.diffouo@prodigyfinance.com>
2019-11-03 22:37:33 +0200

    chore: small changes to the Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import { Link } from 'gatsby'
-
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -13,34 +13,44 @@ import NavItem from './NavItem'
 
 import "./layout.css"
 
-const navLinks = [
-  { to: '/about', text: 'About', cssClass: '', icon: '', ariaLabel: 'Read more about me' },
-  { to: '/', text: 'Blog', cssClass: '', icon: '', ariaLabel: 'Visit my blog' },
-  { to: '//github.com/diraulo', cssClass: 'github', icon: faGithub, ariaLabel: 'View my GitHub profile' },
-  { to: '//twitter.com/diraulo', cssClass: 'twitter', icon: faTwitter, ariaLabel: 'Read some of my tweets' },
-  { to: '//za.linkedin.com/in/diraul', cssClass: 'linkedin', icon: faLinkedinIn, ariaLabel: 'View my LinkedIn profile' }
-]
+const Layout = ({ children }) => {
+  const navLinks = [
+    { to: '/about', text: 'About', cssClass: '', icon: '', ariaLabel: 'Read more about me' },
+    { to: '/', text: 'Blog', cssClass: '', icon: '', ariaLabel: 'Visit my blog' },
+    { to: '//github.com/diraulo', cssClass: 'github', icon: faGithub, ariaLabel: 'View my GitHub profile' },
+    { to: '//twitter.com/diraulo', cssClass: 'twitter', icon: faTwitter, ariaLabel: 'Read some of my tweets' },
+    { to: '//za.linkedin.com/in/diraul', cssClass: 'linkedin', icon: faLinkedinIn, ariaLabel: 'View my LinkedIn profile' }
+  ]
 
-export default ({ children }) => (
-  <div style={{ margin: '3rem auto', maxWidth: 650, padding: '0 1rem' }}>
-    <header style={{ marginBottom: '1.5rem' }}>
-      <Link to="/" style={{ textDecoration: 'none', backgroundImage: 'none' }}>
-        <h3 style={{ display: 'inline', fontStyle: 'normal' }}>Raoul Diffouo</h3>
-      </Link>
+  return (
+    <>
+      <Helmet>
+        <html lang="en"></html>
+      </Helmet>
 
-      <ul style={{ listStyle: 'none', marginLeft: '0' }}>
-        {
-          navLinks.map(({ to, text, cssClass, icon, ariaLabel }) => {
-            return (
-              <NavItem key={text || cssClass} to={to} className={cssClass} ariaLabel={ariaLabel}>
-                {icon !== '' ? <FontAwesomeIcon icon={icon} width="0" /> : text}
-              </NavItem>
-            )
-          })
-        }
-      </ul>
-    </header>
+      <div style={{ margin: '3rem auto', maxWidth: 650, padding: '0 1rem' }}>
+        <header style={{ marginBottom: '1.5rem' }}>
+          <Link to="/" style={{ textDecoration: 'none', backgroundImage: 'none' }}>
+            <h3 style={{ display: 'inline', fontStyle: 'normal' }}>Raoul Diffouo</h3>
+          </Link>
 
-    {children}
-  </div>
-)
+          <ul style={{ listStyle: 'none', marginLeft: '0' }}>
+            {
+              navLinks.map(({ to, text, cssClass, icon, ariaLabel }) => {
+                return (
+                  <NavItem key={text || cssClass} to={to} className={cssClass} ariaLabel={ariaLabel}>
+                    {icon !== '' ? <FontAwesomeIcon icon={icon} width="0" /> : text}
+                  </NavItem>
+                )
+              })
+            }
+          </ul>
+        </header>
+
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default Layout
