77b91181ebd159905c1461e2af604fa09eed92a3
Raoul DIFFOUO <raoul.diffouo@prodigyfinance.com>
2019-11-03 12:39:54 +0200

    a11y: add labels to navigation links

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link as GatsbyLink } from 'gatsby'
 
-const Link = ({ children, to, className }) => {
+const Link = ({ children, to, className, ariaLabel }) => {
   const internal = /^\/(?!\/)/.test(to)
 
   return (
     internal
-      ? <GatsbyLink to={to} className={className}>{children}</GatsbyLink>
-      : <a href={to} className={className}>{children}</a>
+      ? <GatsbyLink to={to} className={className} aria-label={ariaLabel}>{children}</GatsbyLink>
+      : <a href={to} className={className} aria-label={ariaLabel}>{children}</a>
   )
 }
 
