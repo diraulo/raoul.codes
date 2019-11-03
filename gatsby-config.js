90be68d37bfd044abd63499aaf05b99473e69a01
Raoul DIFFOUO <raoul.diffouo@prodigyfinance.com>
2019-11-03 12:50:17 +0200

    chore: remove gatsby manifest for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,18 +56,6 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        // icon: '', // This path is relative to the root of the site.
-      },
-    },
     'gatsby-plugin-react-helmet',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
